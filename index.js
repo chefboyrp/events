9e55909f2ee8f065d75c31c7e145c323a073eec0
Ryan Murphy <ryanmurphy@stripe.com>
2019-04-16 18:27:26 -0700

    final commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("yo world")
+
+    let changeColorButton = document.querySelector('#changeColor')
+    let colorDiv = document.querySelector('#colorDiv')
+    let clickCount = 0
+
+
+    changeColorButton.addEventListener('click', function(){
+
+        clickCount = clickCount + 1
+
+        if (clickCount % 2 == 0) {
+
+            colorDiv.style.background = 'green'
+            clickCount = 0
+            
+        } else {
+            colorDiv.style.background = 'white'
+            
+        }
+
+
+        console.log(clickCount)
+
+ 
+    })
+
+    
+    changeColorButton.addEventListener('mouseover', function(){
+
+        colorDiv.style.background = 'pink'
+
+    })
+
+    
+
+})
+
+function changeColorDiv(){
+    colorDiv.style.background = 'red'
+  }
+
+
+
